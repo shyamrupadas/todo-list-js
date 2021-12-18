77593b904dd7031c7ec1d974b9928e6ef8119c11
Sergey Maksimov <s.mksimov@gmail.com>
2021-12-18 22:16:14 +0300

    add: create todo

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const todoForm = document.getElementById('add-todo');
 const todoUl = document.getElementById('todo-items');
+const inputTodo = document.getElementById('todo-text')
 
 const addTodo = event => {
   event.preventDefault();
-  // Todo добавление нового todo
-  alert('Add new todo');
+  const liEl = document.createElement('li');
+  liEl.append(inputTodo.value);
+  todoUl.append(liEl);
+
+  // Todo проверка, что в поле ввода что-то введено
+
 };
 
 todoForm.addEventListener('submit', event => addTodo(event));
