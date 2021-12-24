b9bc38e20f6477b76e5ec250771bb184eafe8ac6
Sergey Maksimov <s.mksimov@gmail.com>
2021-12-24 23:20:34 +0300

    refactor: delete button

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const todoForm = document.getElementById('add-todo');
 const todoUl = document.getElementById('todo-items');
-const inputTodo = document.getElementById('todo-text')
+const inputTodo = document.getElementById('todo-text');
 
-const addTodo = event => {
-  event.preventDefault();
-  if (inputTodo.value) {
-    const liEl = document.createElement('li');
-    liEl.append(inputTodo.value);
-    todoUl.append(liEl);
-    inputTodo.value = '';
-  }
+const deleteTodo = todoEL => {
+  // Todo удаление todo
+
 };
 
-todoForm.addEventListener('submit', event => addTodo(event));
+const addTodo = () => {
+  const liEl = document.createElement('li');
+  liEl.append(inputTodo.value);
+
+
+  const deleteButtonEl = document.createElement('button');
+  deleteButtonEl.append('x');
+  liEl.append(deleteButtonEl);
+
+  todoUl.append(liEl);
+  deleteButtonEl.addEventListener('click', event => deleteTodo(event));
+};
+
+const addTodoHandler = event => {
+  event.preventDefault();
+  if (!inputTodo.value) return;
+
+  addTodo(inputTodo.value);
+  inputTodo.value = '';
+};
+
+todoForm.addEventListener('submit', event => addTodoHandler(event));
